bc8d3b2a97bbd8db5bc70e37f7d25292eea05f53
Tre' Seymour <wayne.seymour@elastic.co>
2020-04-30 16:46:14 -0600

    Add debug

--- src/dev/code_coverage/shell_scripts/ingest_coverage.sh
+++ src/dev/code_coverage/shell_scripts/ingest_coverage.sh
@@ -17,11 +17,18 @@ export ES_HOST
 STATIC_SITE_URL_BASE='https://kibana-coverage.elastic.dev'
 export STATIC_SITE_URL_BASE
 
+echo "### List Dir"
+ls .
+
 for x in jest functional mocha; do
   echo "### Ingesting coverage for ${x}"
 
+  echo "### Debug"
+  head -10 target/kibana-coverage/${x}-combined/coverage-summary.json || echo "### Something wrong with ${x}'s summary file?"
+
   COVERAGE_SUMMARY_FILE=target/kibana-coverage/${x}-combined/coverage-summary.json
 
+  head -10 VCS_INFO.txt || echo "### Something wrong with the VCS_INFO text file?"
   node scripts/ingest_coverage.js --verbose --path ${COVERAGE_SUMMARY_FILE} --vcsInfoPath ./VCS_INFO.txt
 done
 
